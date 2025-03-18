cf188b667b6dae65d55dbedfeaeed57ba065a05b
santhok187 <128885479+santhok187@users.noreply.github.com>
2025-03-18 17:31:41 +0530

    update transofmr

--- transformation.py
+++ transformation.py
@@ -6,3 +6,16 @@ float_value = float(string_value)
 print(float_value)
 # Output
 123.45
+
+
+#transform int to bigint
+# Example integer
+integer_value = 1234567890  # 1,234,567,890 in thousands separator  1,234,567,890   
+# Convert integer to bigint
+bigint_value = int(integer_value)
+# Print the result
+print(bigint_value)
+# Output
+
+1234567890
+#transform int to bigint
